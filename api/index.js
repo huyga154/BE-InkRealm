d2d5b72cc2a1c1926e3f57826298b48f34a6ab7c
HeaterMon <87904983+huyga154@users.noreply.github.com>
2025-09-25 12:12:16 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,14 +1,6 @@
-const express = require("express");
-const indexRouter = require("../routes/index");
-const { swaggerUi, swaggerSpec } = require("./swagger");
+const app = require("../app");
 
-const app = express();
-
-// Mount routes
-app.use("/", indexRouter);
-
-// Swagger UI tại /api-docs
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-// Export cho Vercel
-module.exports = app;
+// Vercel cần export handler function
+module.exports = (req, res) => {
+    app(req, res);
+};
