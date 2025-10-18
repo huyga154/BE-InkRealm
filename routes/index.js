4d8f2285f7d04300b15d83d529da1fbe98b78e36
HeaterMon <87904983+huyga154@users.noreply.github.com>
2025-10-18 16:25:04 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
+require('dotenv').config();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
     res.send("Hello from index route");
     console.log("here is router // index.js ")
-    res.redirect('/api-docs');
+    res.redirect(process.env.BACKEND_URL + '/api-docs');
 });
 
 module.exports = router;
